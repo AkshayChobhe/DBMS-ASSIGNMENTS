4917e528b65fee4aa71adb288c5a3cd43f298ae5
AkshayChobhe <91774366+AkshayChobhe@users.noreply.github.com>
2022-10-31 14:34:05 +0530

    DDL, DML Commands

--- Assignemnt_1-1.sql
+++ Assignemnt_1-1.sql
@@ -14,7 +14,6 @@ Copyright (c) 2000, 2022, Oracle and/or its affiliates.
 Oracle is a registered trademark of Oracle Corporation and/or its
 affiliates. Other names may be trademarks of their respective
 owners.
-
 Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
 
 mysql> show databases;
